61d274d0347cb8f0803b550b9d247555610858a5
wangdongman <wangdongman@xingfeiinc.com>
2021-01-14 21:02:02 +0800

    update: 更新Card 组件UI

---
+++ src/renderer/components/Card/index.vue
@@ -0,0 +1,71 @@
+<!--
+ vue file
+ -->
+<template>
+    <div class="card__wrap">
+        <div class="card">
+            <div class="card__head"></div>
+            <div class="card__body">
+                <p class="card__name">{{item.gameName}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'card',
+    components: {},
+    props: {
+      item: Object
+    },
+    data () {
+      return {
+      }
+    },
+    watch: {},
+    computed: {},
+    created () {
+    },
+    beforeDestroy () {
+    },
+    mounted () {
+    },
+    methods: {}
+  }
+</script>
+
+<style type="text/scss" lang="scss">
+    .card {
+        &__wrap {
+            width: 25%;
+            padding: 0 .5em 1em;
+            overflow: hidden;
+        }
+
+        border-radius: 4px;
+        background-color: #fff;
+        box-shadow: 0 6px 7px 0 rgba(131, 125, 125, 0.13);
+
+        &:hover {
+            .card__name {
+                color: #ffffff;
+                background-color: #419BF9;
+            }
+        }
+
+        &__head {
+            padding-top: 56%;
+            background-color: #ccc;
+        }
+        &__body {
+            height: 2.5em;
+            line-height: 2.5em;
+        }
+        &__name {
+            padding: 0 1em;
+            color: #24273F;
+            font-size: 18px;
+        }
+    }
+</style>
--- src/renderer/components/main/index.vue
+++ src/renderer/components/main/index.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="x-main">
-    <button @click.stop="onCreateScan">新建导入游戏</button>
+    <button @click.stop="onCreateScan">新建存档游戏</button>
     <button @click.stop="onExportSave">导出扫描文件</button>
-    <card :key="item.gameName" v-for="item in scanList" :item="item"></card>
-
+     <div class="card-box">
+         <card :key="item.gameName" v-for="item in scanList" :item="item"></card>
+     </div>
     <new-scan v-model.sync="dialogFormVisible" @handleExit="handleExit" @handelSubmit="handelSubmit"/>
   </div>
 </template>
 
 <script>
     /* eslint-disable */
-  import card from '../card'
+  import card from '../Card'
   import newScan from '../new-scan.vue'
   import eventMessage from '../../mixins/eventMessage'
   export default {
@@ -22,7 +23,45 @@
     data () {
       return {
         dialogFormVisible: false,
-        scanList: []
+        scanList: [
+          {
+            // [游戏名]
+            "gameName": '空洞骑士',
+            // 游戏存档目录名
+            "gameDocDir": 'Hollow Knight',
+            // [游戏开发公司/团队]
+            "gameCompany": 'Team Cherry',
+            // [游戏存档路径] 自动计算?后面跟进大数据尝试识别
+            "gameDocPath": '/Team Cherry/Hollow Knight',
+            // 游戏存档路径
+            "fullPath": '',
+            // 游戏主程序路径
+            "mainProgram": '',
+            // 游戏目录
+            "gameDir": '',
+            // 启动方式
+            "startMode": '',
+            // 游戏平台
+            "gamePlatform": [
+              {
+                id: '',
+                name: 'steam',
+                link: 'steam://rungameid/game-id'
+              },
+              {
+                id: '',
+                name: 'epic',
+                link: ''
+              }
+            ],
+            // 游戏版本
+            "version": "",
+            // 最后备份时间
+            "lastBackTime": '',
+            // 备份路径(多路径,因为支持多路径的)
+            "backPath": []
+          }
+        ]
       }
     },
     mounted () {
@@ -53,6 +92,16 @@
   }
 </script>
 
-<style>
+<style type="text/scss" lang="scss">
+    .x-main {
+        padding: 1em;
+        background-color: #f7f7f7;
+    }
 
+    .card-box {
+        display: flex;
+        flex-flow: row wrap;
+        align-content: flex-start;
+        justify-content: flex-start;
+    }
 </style>
