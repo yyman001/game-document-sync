e09d89e43b1ff5c9b7038ed20b032c4f225528ea
wangdongman <wangdongman@xingfeiinc.com>
2021-01-15 18:31:29 +0800

    update: 获取用户目录

---
+++ src/utils/systemUserInfo.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+/**
+ * 获取系统用户名, 用户名目录
+ *
+ * @returns {Object} - 用户信息
+ * @eg
+  {
+    uid: -1,
+    gid: -1,
+    username: 'XF',
+    homedir: 'C:\\Users\\XF',
+    shell: null
+  }
+ */
+function getSystemUserInfo() {
+  return os.userInfo()
+}
+
+module.exports = {
+  getSystemUserInfo
+}
