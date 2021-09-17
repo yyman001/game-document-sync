2e439723c022b66c7b3974abf7d46252bfe7081f
yyman001 <yyman001@qq.com>
2021-09-17 16:54:00 +0800

    update: 启用@vue/composition-api

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,11 +8,13 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '../utils/db'
+import VueCompositionAPI from '@vue/composition-api'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.use(VueCompositionAPI)
 Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
