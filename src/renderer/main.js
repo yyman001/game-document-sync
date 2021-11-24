27b7149e2ac2a467b9cba96f987e97a63b7d97f7
yyman001_cp <yyman001@qq.com>
2021-11-24 20:24:25 +0800

    add: ant-design-vue

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -9,6 +9,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '../utils/db'
 import VueCompositionAPI from '@vue/composition-api'
+import antDesignVue from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
@@ -16,6 +18,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueCompositionAPI)
 Vue.use(ElementUI)
+Vue.use(antDesignVue)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
