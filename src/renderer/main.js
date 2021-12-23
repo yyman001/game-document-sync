59f3e5c97a8b00c38730ae8c7053c9b8481bcd70
yyman001 <yyman001@qq.com>
2021-12-23 23:36:31 +0800

    del: 移除 pinia, electron 中无法正常使用

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -11,13 +11,10 @@ import './utils/DexieDB'
 import VueCompositionAPI from '@vue/composition-api'
 import antDesignVue from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import { createPinia, PiniaVuePlugin } from 'pinia'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-const pinia = createPinia()
-Vue.use(PiniaVuePlugin)
 Vue.use(VueCompositionAPI)
 Vue.use(ElementUI)
 Vue.use(antDesignVue)
@@ -26,6 +23,5 @@ new Vue({
   components: { App },
   router,
   store,
-  pinia,
   template: '<App/>'
 }).$mount('#app')
