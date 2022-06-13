a68f6de835dd1dc8af53023e3c4e6ed11a4c3689
yyman001_cp <yyman001@qq.com>
2022-06-13 18:37:34 +0800

    update: 读取本地文件

---
+++ packages/renderer/src/hooks/file/useLocalBackupFile.ts
@@ -0,0 +1,74 @@
+import { ref, computed, onMounted, unref } from 'vue'
+import { getDirectoryItem } from '@/utils/tools'
+import path from 'path'
+
+export default function () {
+  const rootDir = ''
+  const directoryItem = ref([])
+  const fileItem = ref([])
+
+  const localDirectoryListName = computed(() => {
+    return unref(directoryItem).map(f => f.basename)
+  })
+
+  // 本地文件名: 格式规范 = (文件夹/文件名) = Aragami/Aragami_t1641735966693.zip
+  const localFileListName = computed(() => {
+    return unref(fileItem).map(f => f.comparsedName)
+  })
+
+  const loadLocalFileDirectoryItem = async () => {
+    try {
+      // TODO: 获取配置的 备份文件夹
+      const filePath = path.join(rootDir, 'backup')
+      const list = await getDirectoryItem(filePath)
+      directoryItem.value = list.filter(f => f.type === 'directory')
+      // 移除第一个备份目录
+      directoryItem.value.shift()
+      fileItem.value = list.filter(f => f.type === 'file').map((f) => {
+        return {
+          ...f,
+          // 用于比较同步文名标识
+          comparsedName: `${f.dirname}/${f.basename}`
+        }
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getDirectoryChildren = (dirname: string) => {
+    return unref(fileItem).filter(f => f.dirname === dirname)
+  }
+
+  const downloadFile = async (file: any, dirname: string) => {
+    // 组成: 配置的存档文件夹/游戏目录/游戏存档文件.后缀
+    // eg: "/games_doc_sync/test/game.file.config.json"
+    const downloadUrl = file.filename
+    // TODO: 备份文件夹名称读配置
+    const filePath = path.join(rootDir.value, 'backup', dirname, file.basename)
+    // TODO: 下载方法迁移到 cloud 模块
+    /* downloadCloudFile(downloadUrl, filePath, () => {
+      file.path = filePath
+      file.dirname = dirname
+      fileItem.value.push(file)
+    }) */
+  }
+
+  onMounted(() => {
+    // TODO: 主线程不支持直接调用node模块
+    // loadLocalFileDirectoryItem()
+  })
+
+  return {
+    fileItem,
+    directoryItem,
+
+    loadLocalFileDirectoryItem,
+    getDirectoryChildren,
+
+    localFileListName,
+    localDirectoryListName,
+
+    downloadFile
+  }
+}
--- packages/renderer/src/view/backup/index.vue
+++ packages/renderer/src/view/backup/index.vue
@@ -1,13 +1,108 @@
 <template>
-  <div>backup</div>
+  <div class="backup">
+    <FileExplorer>
+      <div class="file-content">
+        <!-- 一级文件夹 -->
+        <template :key="item.basename" v-for="item in directoryItem">
+          <FileItem
+            v-show="!activeDirectoryName"
+            :fileName="item.basename"
+            :fileType="item.type"
+            :fileSize="formatFileSize(folderSize(item.basename))"
+            :item="item"
+            :time="formatTimestamp(item.timeStamp, 'YYYY-MM-DD HH:mm')"
+            :isSyncSuccess="false"
+            :isCloudFile="!item.path"
+            :disabled="true"
+          />
+        </template>
+
+        <!-- 二级文件列表 -->
+        <FileItem
+          :key="item.basename"
+          :fileType="item.type"
+          :fileName="item.basename"
+          :fileSize="formatFileSize(item.size)"
+          :time="formatTimestamp(item.timeStamp, 'YYYY-MM-DD HH:mm')"
+          :item="item"
+          :isSyncSuccess="false"
+          :isCloudFile="!item.path"
+          :disabled="false"
+          v-for="item in fileList"
+        />
+      </div>
+    </FileExplorer>
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { computed, defineComponent, ref, unref } from 'vue'
+import FileExplorer from '@/components/FileExplorer/index.vue'
+import FileItem from '@/components/FileExplorer/FileItem.vue'
 
-}
+import useLocalBackupFile from '@/hooks/file/useLocalBackupFile'
+
+import { formatTimestamp } from '@/utils/formatTimestamp'
+import { formatFileSize } from '@/utils/formatFileSize'
+
+export default defineComponent({
+  components: { FileExplorer, FileItem },
+  props: {
+    searchText: String
+  },
+  setup (props) {
+    const {
+      directoryItem,
+      fileItem,
+      localDirectoryListName,
+      localFileListName,
+      getDirectoryChildren
+    } = useLocalBackupFile()
+
+    // 当前打开的文件夹
+    const activeDirectoryName = ref<string>('')
+    const handleSetDirectory = (directoryName = '') => {
+      activeDirectoryName.value = directoryName
+    }
+
+    // 本地文件列表 + 云文件列表
+    const getChildrenByLocalAndCloud = (gameDocDir: string) => {
+      if (!gameDocDir) return []
+
+      const localFile = getDirectoryChildren(gameDocDir)
+      const cloudFile = []
+
+      return localFile.concat(cloudFile)
+    }
+
+    const fileList = computed(() => {
+      return getChildrenByLocalAndCloud(unref(activeDirectoryName))
+    })
+
+    // TODO
+    const folderSize = (directoryName: string) => {
+      return 0
+    }
+
+    return {
+      activeDirectoryName,
+      handleSetDirectory,
+
+      directoryItem,
+      fileItem,
+      localDirectoryListName,
+      localFileListName,
+      getDirectoryChildren,
+
+      fileList,
+      folderSize,
+
+      formatTimestamp,
+      formatFileSize
+    }
+  }
+})
 </script>
 
-<style>
-
+<style lang="sass" scoped>
 </style>
