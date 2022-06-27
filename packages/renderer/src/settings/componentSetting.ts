466947425552c42a6abf9f032b335f7e96bc0fad
yyman001_cp <yyman001@qq.com>
2022-06-27 16:59:30 +0800

    update: 滚动插件&声明&一些工具类

---
+++ packages/renderer/src/settings/componentSetting.ts
@@ -0,0 +1,10 @@
+// Used to configure the general configuration of some components without modifying the components
+
+export default {
+  // scrollbar setting
+  scrollbar: {
+    // Whether to use native scroll bar
+    // After opening, the menu, modal, drawer will change the pop-up scroll bar to native
+    native: false
+  }
+}
